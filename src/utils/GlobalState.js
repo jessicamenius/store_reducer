d4d6d82f073b29e0a925de15f2066a7fa905f869
Jessica <jessicamenius@gmail.com>
2020-08-15 15:07:09 -0500

    first commit

---
+++ src/utils/GlobalState.js
@@ -0,0 +1,44 @@
+import React, { createContext, useReducer, useContext } from "react";
+
+const TodoContext = createContext();
+
+const { Provider } = TodoContext;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        { id: state.length * Math.random(), name: action.name },
+      ];
+
+    case "remove":
+      return state.filter((_, index) => {
+        return index !== action.index;
+      });
+
+    case "prioritize":
+      return state.map((item, index) => {
+        if (index === action.index) {
+          return { ...item, priority: !item.priority };
+        }
+        return item;
+      });
+
+    default:
+      return state;
+  }
+}
+
+function TodoProvider({ ...props }) {
+  const [state, dispatch] = useReducer(reducer, []);
+
+  console.log("This is a props of the highest order", props);
+  return <Provider value={[state, dispatch]} {...props} />;
+}
+
+function useTodoContext() {
+  return useContext(TodoContext);
+}
+
+export { TodoProvider, useTodoContext };
